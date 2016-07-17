d1ea15628f7d904a71c7547099871f72b08be5f4
Akshaya <akkshayya@gmail.com>
2016-07-17 20:59:13 +0530

    Update test_case1.py

--- test_case1.py
+++ test_case1.py
@@ -5,18 +5,23 @@ from final import find_matches
 from final import getchoice
 from final import select_match
 
-
-
 class mytest(unittest.TestCase):
-
+	def Test_get_url(self):  #test the geturl method
+		self.assertEqual(get_url(),'item')
+	
+	def Test_find_matches(self): #tests the list of matches 
+		self.assertEqual(find_matches('description'),'Latest scores from Cricinfo')
+		self.assertEqual(find_matches('title'),'Cricinfo Live Scores')
 	
 	
-	def test_get_choice(self):
-		self.assertEqual(type(getchoice()),int)
-		
+	def Test_select_match(self):  #test the operation on selecting a match
+		self.assertEqual(select_match(),1)
+		self.assertEqual(select_match(),2)
+		self.assertEqual(select_match(),3)
+		self.assertEqual(select_match(),100)
+		self.assertEqual(select_match(),yes)
+	
+	def Test_signal_handler(self):
+		self.assertEqual(signal_handler()),'')
 
-	def test_select_match(self):
-		self.assertEqual(type(select_match()),int)
-		
-
-unittest.main()
+Unittest.main()
